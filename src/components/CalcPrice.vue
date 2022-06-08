43a11a77c2406f3be7aa33a009c2b608f69730a3
luckypeppa <871565198@qq.com>
2022-06-08 15:16:22 +0800

    Add some of element in CalcPrice

---
+++ src/components/CalcPrice.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container fluid class="d-flex flex-column align-center">
+    <h1 class="text-center">ModelS</h1>
+    <p class="text-center">Est. Delivery: Nov 2022 - Feb 2023</p>
+    <!-- <v-btn-toggle
+      v-model="toggleSaving"
+      class="toggle-saving d-flex grey lighten-3 rounded-xl pa-1"
+    >
+      <v-btn small text class="rounded-xl" :outlined="false">
+        Purchase Price
+      </v-btn>
+      <v-btn small text class="rounded-xl"> Potential Saving </v-btn>
+    </v-btn-toggle> -->
+    <div
+      class="toggle-saving d-flex grey lighten-3 rounded-xl pa-1"
+      active-class="btn-active"
+    >
+      <v-btn
+        small
+        text
+        :class="{
+          'rounded-xl': 'rounded-xl',
+          'btn-active': showSaving === false,
+        }"
+        @click="showSaving = false"
+      >
+        Purchase Price
+      </v-btn>
+      <v-btn
+        small
+        text
+        :class="{
+          'rounded-xl': 'rounded-xl',
+          'btn-active': showSaving === true,
+        }"
+        @click="showSaving = true"
+      >
+        Potential Saving
+      </v-btn>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showSaving: false,
+    };
+  },
+  //   methods: {
+  //     toggleSaving(boolean) {},
+  //   },
+};
+</script>
+
+<style lang="scss" scoped>
+// .prices {
+//   width: 80%;
+// }
+
+.toggle-saving {
+}
+
+.btn-active {
+  background-color: #fff;
+}
+</style>
--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,21 +1,40 @@
 <template>
-  <v-main>
-    <v-container fluid>
-      <v-row>
-        <v-col cols="8">
-          <app-slider />
-        </v-col>
-        <v-spacer></v-spacer>
-      </v-row>
-    </v-container>
-  </v-main>
+  <v-container fluid class="order-container pt-16">
+    <v-row>
+      <v-col cols="9">
+        <app-slider />
+      </v-col>
+      <v-col cols="3" class="px-12">
+        <div
+          class="message pt-5 d-flex align-start grey lighten-3 px-5 rounded-lg mx-auto mb-16"
+        >
+          <v-icon color="blue" class="mr-6">mdi-alert-circle</v-icon>
+          <p>
+            $750 Clean Fuel Reward is now available for California residents.
+          </p>
+        </div>
+        <calc-price></calc-price>
+      </v-col>
+      <v-spacer></v-spacer>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import AppSlider from "@/components/AppSlider.vue";
+import CalcPrice from "@/components/CalcPrice.vue";
 export default {
-  components: { AppSlider },
+  components: { AppSlider, CalcPrice },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.order-container {
+  height: 100vh;
+  overflow: hidden;
+
+  .message {
+    width: 100%;
+  }
+}
+</style>
